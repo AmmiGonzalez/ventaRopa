9f3b56122e04c25b158346cdff8335cd72beebd6
Ammi González <64441015+AmmiGonzalez@users.noreply.github.com>
2023-05-30 15:59:11 -0600

    Cambios en Frontend

--- clientes/index.js
+++ clientes/index.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import "./database/connectdb.js"
 import express from "express";
 import clientRoutes from "./routes/client.router.js"
+import cors from "cors"
 
 const app=express();
 app.use(express.json())
+app.use(cors())
 app.use('/clientes', clientRoutes)
 
 const PORT = process.env.PORT || 5000
--- frontend/src/pages/Clientes.svelte
+++ frontend/src/pages/Clientes.svelte
@@ -1,7 +1,107 @@
 <script>
+  let cliente = {
+    NIT: "",
+    Nombre: "",
+    FechaNacimiento: "",
+    Telefono: "",
+    Correo: ""
+  };
+  async function registroCliente(){
+    const response = await fetch('http://localhost:5000/clientes/nuevoCliente', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cliente)
+    });
 
+    if (response.ok) {
+      const createdClient = await response.json();
+      console.log('Cliente registrado:', createdClient);
+      // Aquí puedes agregar tu lógica para manejar la respuesta exitosa
+    } else {
+      const errorData = await response.json();
+      console.error('Error al registrar el cliente:', errorData);
+      // Aquí puedes agregar tu lógica para manejar el error
+    }
+  }
 </script>
 
 <div class="clientesContainer">
-  Hola desde clientes
+ <h1>Clientes</h1>
+
+  <form on:submit|preventDefault={registroCliente}>
+    <label for="address" class="formbold-form-label">
+      NIT:
+    </label>
+    <input
+        bind:value={cliente.NIT}
+        type="text"
+        name="NIT"
+        id="NIT"
+        class="formbold-form-input"
+        required
+    />
+    <br>
+    
+    
+    <label for="address" class="formbold-form-label">
+      Nombre:
+    </label>
+    <input
+      bind:value={cliente.Nombre}
+      type="text"
+      name="Nombre"
+      id="Nombre"
+      class="formbold-form-input"
+      required
+    />
+    <br>
+    
+    
+    <label for="address" class="formbold-form-label">
+      Fecha Nacimiento:
+    </label>
+    <input
+      bind:value={cliente.FechaNacimiento}
+      type="date"
+      name="FechaNacimiento"
+      id="FechaNacimiento"
+      class="formbold-form-input"
+      required
+    />
+    <br>
+    
+    
+    <label for="address" class="formbold-form-label">
+      Telefono:
+    </label>
+    <input
+      bind:value={cliente.Telefono}
+      type="text"
+      name="Telefono"
+      id="Telefono"
+      class="formbold-form-input"
+      required
+    />
+    <br>
+    
+    <label for="address" class="formbold-form-label">
+      Correo:
+    </label>
+    <input
+      bind:value={cliente.Correo}
+      type="text"
+      name="Correo"
+      id="Correo"
+      class="formbold-form-input"
+      required
+    />
+    <br>
+    <button class="formbold-btn">Registrar </button>
+  </form>
 </div>
+
+<style>
+
+</style>
